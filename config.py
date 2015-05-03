37866afac4d8d64d8db24bbf5ad03ac4f87c69d8
Oldrich Vykydal <info@oldrichvykydal.cz>
2015-05-03 14:00:08 +0200

    a small config fix

---
+++ config.py
@@ -0,0 +1,14 @@
+"""Module for main configuration"""
+
+import os
+
+# Set complete name with domain
+SERVER_NAME = 'localhost:5000'
+
+# IIIF server
+IIIF_SERVER = 'http://iiifhawk.klokantech.com'
+
+# Database config - you don't need to change it
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
