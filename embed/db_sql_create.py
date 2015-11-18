34b913dd7de4c8cc0152df0951e20c64d80a6b85
o1da <info@oldrichvykydal.cz>
2015-11-18 15:46:02 +0100

    sqlite db creation bug fix

--- embed/db_sql_create.py
+++ embed/db_sql_create.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('data/sql/db.db')
 c = conn.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS Batch (batch_id INTEGER PRIMARY KEY AUTOINCREMENT, batch_data TEXT)")
 c.execute("CREATE TABLE IF NOT EXISTS Task (task_id INTEGER, batch_id INTEGER, item_id VARCHAR(256), status VARCHAR(16), url TEXT, message INTEGER)")
-c.execute("CREATE INDEX test_index ON Task (batch_id, item_id)")
+c.execute("CREATE INDEX IF NOT EXISTS test_index ON Task (batch_id, item_id)")
 
 conn.commit()
 conn.close()
